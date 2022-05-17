1b12ab82486e37370ac2a9f4b4201ecaed901835
Martin Kearn <martin.kearn@microsoft.com>
2022-05-17 16:43:19 +0100

    Remove weather and add basic test

--- src/DMR.Api.UnitTests/UnitTest1.cs
+++ src/DMR.Api.UnitTests/UnitTest1.cs
@@ -5,10 +5,12 @@ namespace DMR.Api.UnitTests
 {
     public class UnitTest1
     {
+        // This just a throw-away test to use until we write proper tests for this repo
         [Fact]
         public void Test1()
         {
-
+            var foo = "bar";
+            Assert.Equal("bar", foo);
         }
     }
 }
