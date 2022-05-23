566e8fd5e65dfa7174290034f95d1bf835ea9342
Priyanka Sharma <prisharma@microsoft.com>
2022-05-23 18:29:32 +0100

    Adding first version of DMR Controller and required models

---
+++ src/Dmr.Api/Controllers/DMRController.cs
@@ -0,0 +1,21 @@
+﻿using Dmr.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dmr.Api.Controllers
+{
+    [Route("/messages")]
+    [ApiController]
+    public class DMRController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] MessagesInput messages, [FromHeader] HeadersInput headers)
+        {
+            if (messages == null || headers == null )
+            {
+                return BadRequest(ModelState);
+            }
+            return Accepted();
+        }
+
+    }
+}
---
+++ src/Dmr.Api/Models/HeadersInput.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Dmr.Api.Models
+{
+    // No logic so no unit tests are required
+    [ExcludeFromCodeCoverage]
+    public class HeadersInput
+    {
+        [FromHeader(Name = "X-Sent-From")]
+        public string? XSentFrom { get; set; }
+
+        [FromHeader(Name = "X-Send-To")]
+        public string? XSentTo { get; set; }
+
+        [FromHeader(Name = "X-Message-Id")]
+        public string? XMessageId { get; set; }
+
+        [FromHeader(Name = "X-Message-Id-Ref")]
+        public string? XMessageIdRef { get; set; }
+
+    }
+}
