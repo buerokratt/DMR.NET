ab59048a94d8cece56eda36622737ddcdf85d710
Priyanka Sharma <prisharma@microsoft.com>
2022-05-26 11:15:10 +0100

    Adding test case for DMR Controller and updating the header field name

--- src/Dmr.Api/Models/HeadersInput.cs
+++ src/Dmr.Api/Models/HeadersInput.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Dmr.Api.Models
 {
-    // No logic so no unit tests are required
-    [ExcludeFromCodeCoverage]
     public class HeadersInput
     {
-        [FromHeader(Name = "X-Sent-From")]
-        public string? XSentFrom { get; set; }
+        [FromHeader(Name = "X-Sent-By")]
+        public string? XSentBy { get; set; }
 
         [FromHeader(Name = "X-Send-To")]
         public string? XSendTo { get; set; }
