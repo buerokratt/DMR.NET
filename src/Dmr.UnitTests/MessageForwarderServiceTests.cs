da407a66d125869d34a65baf68b501cce717e0a0
Guy Pritchard <Guy.Pritchard@cdgad.onmicrosoft.com>
2022-05-27 17:40:40 +0100

    Removing un-needed namespace

--- src/Dmr.UnitTests/MessageForwarderServiceTests.cs
+++ src/Dmr.UnitTests/MessageForwarderServiceTests.cs
@@ -7,7 +7,6 @@ using RichardSzalay.MockHttp;
 using System;
 using System.Net;
 using System.Net.Http;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Xunit;
 
