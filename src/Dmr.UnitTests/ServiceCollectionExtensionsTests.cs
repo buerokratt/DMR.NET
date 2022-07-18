efe457f1aab06e046bd9cf09a1c9c73dfa23eb05
Guy Pritchard <Guy.Pritchard@cdgad.onmicrosoft.com>
2022-07-18 12:01:18 +0100

    Adding participant poller config.

--- src/Dmr.UnitTests/ServiceCollectionExtensionsTests.cs
+++ src/Dmr.UnitTests/ServiceCollectionExtensionsTests.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Dmr.Api.Services.MessageForwarder;
 using System;
 using Dmr.Api.Utils;
+using Buerokratt.Common.CentOps;
 
 namespace Dmr.UnitTests
 {
@@ -25,13 +26,41 @@ namespace Dmr.UnitTests
             var services = new ServiceCollection();
             var settings = new MessageForwarderSettings();
 
-            //Act
+            // Act
             ServiceCollectionExtensions.AddMessageForwarder(services, settings);
 
-            //Assert
+            // Assert
             Assert.Contains(services, service => service.ServiceType.Name == "MessageForwarderSettings");
             Assert.Contains(services, service => service.ServiceType.Name == "AsyncProcessorSettings");
             Assert.Contains(services, service => service.ServiceType.Name.Contains("IAsyncProcessorService", StringComparison.InvariantCultureIgnoreCase));
+        }
+
+
+        [Fact]
+        public void AddParticipantPollerThrowsforNullSettings()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+
+            //Act & Assert
+            _ = Assert.Throws<ArgumentNullException>(() => ServiceCollectionExtensions.AddParticipantPoller(services, null));
+        }
+
+        [Fact]
+        public void AddParticipantPollerSettingsAdded()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            var settings = new CentOpsServiceSettings();
+
+            // Act
+            ServiceCollectionExtensions.AddParticipantPoller(services, settings);
+
+            // Assert
+            Assert.Contains(services, service => service.ServiceType.Name == "CentOpsServiceSettings");
+            Assert.Contains(services, service => service.ServiceType.Name == "IHostedService");
+            Assert.Contains(services, service => service.ServiceType.Name == "ICentOpsService");
+            Assert.Contains(services, service => service.ServiceType.Name.Contains("ConcurrentDictionary", StringComparison.InvariantCultureIgnoreCase));
         }
     }
 }
